278403af77736b7f5525adf3f96e31b8cf098fc9
Mehdi Seifi <mehdiseifi@gmail.com>
2025-08-07 16:49:29 +0200

    updated dependencies and install scripts

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #! /bin/sh
 # This script will install FeatureForest and its dependencies in a new conda environment.
-echo "\nCreate a new conda environment named featureforest with Python 3.10"
+echo "\nCreate a new conda environment named tree with Python 3.10"
 conda init
-conda create -n featureforest -y python=3.10
-echo "\nActivate the featureforest environment"
+conda create -n tree -y python=3.10
+echo "\nActivate the tree environment"
 source activate base
-conda activate featureforest
+conda activate tree
 echo "\nInstall PyTorch and torchvision using light-the-torch"
 python -m pip install light-the-torch
 ltt install 'torch>=2.5.1' 'torchvision>=0.20.1'
 echo "\nInstall additional dependencies"
+conda install -c conda-forge dask
 python -m pip install -r ./requirements.txt
 echo "\nInstall FeatureForest from GitHub"
 python -m pip install git+https://github.com/juglab/featureforest.git
